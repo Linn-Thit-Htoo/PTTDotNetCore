adbd10c3be527267cfb10e0b5d85fb2b67e69461
Linn Thit <linnthit77387@gmail.com>
2024-01-03 20:40:12 +0630

    Day 1

---
+++ PTTDotNetCore.ConsoleAppDay1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PTTDotNetCore.ConsoleAppDay1
+{
+    public class Program
+    {
+        public static void Main(string[] args) // main method which is the entry point of the program.
+        {
+            
+            Greeting();
+
+            Console.ReadKey(); // if found any key, the program will be stopped.
+        }
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello User!");
+        }
+    }
+}
